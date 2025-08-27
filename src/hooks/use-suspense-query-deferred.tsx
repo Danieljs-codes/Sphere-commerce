b88cc26c586494c3e0d499dfbf043613f877ec77
Olamide <onabiyiolamide13@gmail.com>
2025-08-27 12:39:05 +0100

    removed use spin delay

--- src/hooks/use-suspense-query-deferred.tsx
+++ src/hooks/use-suspense-query-deferred.tsx
@@ -1,36 +1,34 @@
-import { useDeferredValue } from 'react'
-import { useDeepCompareMemo } from 'use-deep-compare'
-import { useSpinDelay } from 'spin-delay'
-
-import { useSuspenseQuery } from '@tanstack/react-query'
 import type {
-  DefaultError,
-  QueryKey,
-  UseSuspenseQueryOptions,
-} from '@tanstack/react-query'
+	DefaultError,
+	QueryKey,
+	UseSuspenseQueryOptions,
+} from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useDeferredValue } from "react";
+import { useDeepCompareMemo } from "use-deep-compare";
 
 export function useSuspenseQueryDeferred<
-  TQueryFnData = unknown,
-  TError = DefaultError,
-  TData = TQueryFnData,
-  TQueryKey extends QueryKey = QueryKey,
+	TQueryFnData = unknown,
+	TError = DefaultError,
+	TData = TQueryFnData,
+	TQueryKey extends QueryKey = QueryKey,
 >(options: UseSuspenseQueryOptions<TQueryFnData, TError, TData, TQueryKey>) {
-  const queryKey = useDeepCompareMemo(
-    () => options.queryKey,
-    [options.queryKey],
-  )
+	const queryKey = useDeepCompareMemo(
+		() => options.queryKey,
+		[options.queryKey],
+	);
 
-  const deferredQueryKey = useDeferredValue(queryKey)
+	const deferredQueryKey = useDeferredValue(queryKey);
 
-  const _query = useSuspenseQuery({
-    ...options,
-    queryKey: deferredQueryKey,
-  })
+	const _query = useSuspenseQuery({
+		...options,
+		queryKey: deferredQueryKey,
+	});
 
-  const query = _query as typeof _query & { isSuspending: boolean }
+	const query = _query as typeof _query & { isSuspending: boolean };
 
-  // 👇 Update this 👇
-  query.isSuspending = useSpinDelay(queryKey !== deferredQueryKey)
+	// 👇 Update this 👇
+	query.isSuspending = queryKey !== deferredQueryKey;
 
-  return query
+	return query;
 }
